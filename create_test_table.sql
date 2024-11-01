4b519f38869b92c8e69ad000e0ede0c6f680fc32
Victoria Forte <victoria.pocladova@gmail.com>
2024-11-01 09:22:28 -0300

    sql para crear tabla de prueba

---
+++ create_test_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `languages` (
+  `repo_name` char(80) DEFAULT NULL,
+  `language_name` char(20) DEFAULT NULL,
+  `language_bytes` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
