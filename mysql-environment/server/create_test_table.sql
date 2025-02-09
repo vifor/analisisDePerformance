d2c50fc16f545bc597939f49a6ac9a9fce10c00a
Victoria Forte <victoria.pocladova@gmail.com>
2025-02-09 14:19:41 -0300

    se quita la implementacion anterior de las pruebas

--- mysql-environment/server/create_test_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `languages` (
-  `repo_name` char(80) DEFAULT NULL,
-  `language_name` char(20) DEFAULT NULL,
-  `language_bytes` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
